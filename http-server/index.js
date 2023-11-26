f5d22cda0e6ef13722dedce701f9181711de6579
Aishwariyaa-Anand <124241367+Aishwariyaa-Anand@users.noreply.github.com>
2023-11-26 15:18:50 +0530

    Create index.js

---
+++ http-server/index.js
@@ -0,0 +1,42 @@
+const http = require("http");
+const fs = require("fs");
+const minimist = require("minimist");
+
+const args = minimist(process.argv.slice(2));
+const port = args.port || 3000;
+
+let homeContent = "";
+let projectContent = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
