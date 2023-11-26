135d601dc12711fc688ae83e441c8bbc8b36a541
Aishwariyaa-Anand <124241367+Aishwariyaa-Anand@users.noreply.github.com>
2023-11-26 16:22:01 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,12 +1,10 @@
 const http = require("http");
 const fs = require("fs");
-const minimist = require("minimist");
-
-const args = minimist(process.argv.slice(2));
-const port = args.port || 3000;
+const args = require("minimist")(process.argv.slice(2));
 
 let homeContent = "";
 let projectContent = "";
+let registrationContent = "";
 
 fs.readFile("home.html", (err, home) => {
   if (err) {
@@ -22,21 +20,38 @@ fs.readFile("project.html", (err, project) => {
   projectContent = project;
 });
 
-http
-  .createServer((request, response) => {
-    let url = request.url;
-    response.writeHeader(200, { "Content-Type": "text/html" });
-    switch (url) {
-      case "/project":
-        response.write(projectContent);
-        response.end();
-        break;
-      default:
-        response.write(homeContent);
-        response.end();
-        break;
+fs.readFile("registration.html", (err, registration) => {
+    if (err) {
+        throw err;
+    }
+    registrationContent = registration;
+});
+
+fs.readFile("home.html", (err, home) => {
+    console.log(home.toString());
+});
+
+fs.readFile("home.html",(err, home) => {
+    if (err) {
+        throw err;
+    }
+    http.createServer((request, response) => {
+        let url = request.url;
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        switch (url) {
+            case "/registration":
+                response.write(registrationContent);
+                response.end();
+                break;
+            case "/project":
+                response.write(projectContent);
+                response.end();
+                break;
+            default:
+                response.write(homeContent);
+                response.end();
+                break;
     }
   })
-  .listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+  .listen(args);
+})
