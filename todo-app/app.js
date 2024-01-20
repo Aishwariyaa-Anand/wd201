21308851568ef02f0c3539ee2ec7ad727e5cfffb
Aishwariyaa-Anand <124241367+Aishwariyaa-Anand@users.noreply.github.com>
2024-01-20 22:04:50 +0530

    added csrf; updated get, post, put

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,22 +1,41 @@
 const express = require("express");
+var csrf = require("tiny-csrf");
 const app = express();
 const { Todo } = require("./models");
+var cookieparser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const path = require("path");
 
 app.use(bodyParser.json());
 
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieparser("shh! some secret string"));
+app.use(csrf("this_should_be_32_character_long",["POST", "PUT", "DELETE"]));
+
 app.set("view engine", "ejs");
 
 app.get("/", async (request, response) => {
   const allTodos = await Todo.getTodos();
+  const overdue = await Todo.overdue();
+  const dueToday = await Todo.dueToday();
+  const dueLater = await Todo.dueLater();
+  const completed = await Todo.completed();
   if( request.accepts("html")) {
     response.render("index", {
-      allTodos
+      allTodos,
+      overdue,
+      dueToday,
+      dueLater,
+      completed,
+      csrfToken: request.csrfToken(),
     });
   } else {
     response.json({
-      allTodos
+      allTodos,
+      overdue,
+      dueToday,
+      dueLater,
+      completed
     })
   }
 });
@@ -25,19 +44,38 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.get("/todos", async function (_request, response) {
   console.log("Processing list of all Todos ...");
+  const allTodos = await Todo.getTodos();
+  const overdue = await Todo.overdue();
+  const dueToday = await Todo.dueToday();
+  const dueLater = await Todo.dueLater();
+  const completed = await Todo.completed();
+  if( request.accepts("html")) {
+    response.render("index", {
+      allTodos,
+      overdue,
+      dueToday,
+      dueLater,
+      completed,
+      csrfToken: request.csrfToken(),
+    });
+  } else {
+    response.json({
+      allTodos,
+      overdue,
+      dueToday,
+      dueLater,
+      completed
+    })
+  }
   // FILL IN YOUR CODE HERE
-  try {
-    // Query the database to get all Todos
+  /*try {
     const todos = await Todo.findAll();
-    // Respond with the list of Todos
     return response.json(todos);
   } catch (error) {
     console.log(error);
     return response.status(500).json({ error: "Internal Server Error" });
   }
-  // First, we have to query our PostgerSQL database using Sequelize to get list of all Todos.
-  // Then, we have to respond with all Todos, like:
-  // response.send(todos)
+  // response.send(todos)*/
 });
 
 app.get("/todos/:id", async function (request, response) {
@@ -50,20 +88,24 @@ app.get("/todos/:id", async function (request, response) {
   }
 });
 
-app.post("/todos", async function (request, response) {
+app.post("/todos", async (request, response) => {
+  console.log("Creating a todo", request.body);
   try {
-    const todo = await Todo.addTodo(request.body);
-    return response.json(todo);
+    await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+    });
+    return response.redirect("/");
   } catch (error) {
     console.log(error);
     return response.status(422).json(error);
   }
 });
 
-app.put("/todos/:id/markAsCompleted", async function (request, response) {
+app.put("/todos/:id", async (request, response) => {
   const todo = await Todo.findByPk(request.params.id);
   try {
-    const updatedTodo = await todo.markAsCompleted();
+    const updatedTodo = await todo.setCompletionStatus(request.body.completed);
     return response.json(updatedTodo);
   } catch (error) {
     console.log(error);
@@ -75,6 +117,12 @@ app.delete("/todos/:id", async function (request, response) {
   console.log("We have to delete a Todo with ID: ", request.params.id);
   // FILL IN YOUR CODE HERE
   try {
+    await Todo.remove(request.params.id);
+    return response.json({ success: true });
+  } catch (error) {
+    return response.status(422).json(error);
+  }
+  /*try {
     // Query the database to delete a Todo by ID
     const deletedTodo = await Todo.destroy({
       where: {
@@ -89,7 +137,7 @@ app.delete("/todos/:id", async function (request, response) {
   }
   // First, we have to query our database to delete a Todo by ID.
   // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
-  // response.send(true)
+  // response.send(true)*/
 });
 
 module.exports = app;
